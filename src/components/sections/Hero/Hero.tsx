06166a9d93753c3e43b3b3417f59b154b20de914
abi <atocreative.et@gmail.com>
2026-03-20 00:33:31 +0300

    git commit -m "got it 1"

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -10,9 +10,7 @@ export default function Hero() {
   const [showModal, setShowModal] = useState(false);
 
   const heroImages = [
-    'https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?auto=format&fit=crop&w=1200&q=80',
-    'https://images.unsplash.com/photo-1488646953014-85cb44e25828?auto=format&fit=crop&w=1200&q=80',
-    'https://images.unsplash.com/photo-1506929562872-bb421503ef21?auto=format&fit=crop&w=1200&q=80'
+    'https://images.unsplash.com/photo-1573404353091-bd68e3010d73?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   ];
 
   const stats = [
